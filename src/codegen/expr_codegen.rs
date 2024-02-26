dfe3116db777d58ca3f001796c96dc16293fc89c
Michael Zinsmeister <m.zinsmeister@live.de>
2024-02-26 01:24:29 +0100

    move expression codegen to different file

---
+++ src/codegen/expr_codegen.rs
@@ -0,0 +1,311 @@
+use std::fmt::Display;
+
+use crate::expr::{Atom, BuiltIn, Expr};
+
+use super::{ir::{ConstValue, DataType}, BoolRef, CGValueRef, CodeGen, GeneratedCode, I64Ref};
+
+fn fold_op(fun: &BuiltIn, l: Atom, r: Atom) -> Option<Atom> {
+    match (fun, l, r) {
+        (BuiltIn::Plus, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l + r)),
+        (BuiltIn::Times, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l * r)),
+        (BuiltIn::Minus, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l - r)),
+        (BuiltIn::Divide, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l / r)),
+        (BuiltIn::Equal, Atom::Num(l), Atom::Num(r)) => Some(Atom::Boolean(l == r)),
+        (BuiltIn::Equal, Atom::Boolean(l), Atom::Boolean(r)) => Some(Atom::Boolean(l == r)),
+        _ => None,
+    }
+}
+
+fn is_commutative(fun: &BuiltIn) -> bool {
+    match fun {
+        BuiltIn::Plus | BuiltIn::Times | BuiltIn::Equal => true,
+        _ => false,
+    }
+}
+
+pub fn get_type(expr: &Expr) -> DataType {
+    match expr {
+        Expr::Constant(Atom::Num(_)) => DataType::I64,
+        Expr::Constant(Atom::Boolean(_)) => DataType::Bool,
+        Expr::Variable(_) => DataType::I64,
+        Expr::Application(fun, _) => {
+            match fun {
+                BuiltIn::Plus | BuiltIn::Minus | BuiltIn::Times | BuiltIn::Divide => {
+                    DataType::I64
+                },
+                BuiltIn::Equal => {
+                    DataType::Bool
+                }
+            }
+        },
+    }
+}
+
+fn fold_constants(fun: &BuiltIn, args: &[Expr]) -> Option<Vec<Expr>> {
+    if is_commutative(fun) {
+        fold_all_constants_commutative(fun, args)
+    } else {
+        // We just merge all the constants that appear in the beginning of the list for now which should be safe to do
+        // assuming that a list of arguments just means folding the arguments using the function
+        let mut result = Vec::new();
+
+        let mut args_iter = args.iter().peekable();
+
+        let data_type = get_type(&args[0]);
+        
+        if let Some(Expr::Constant(n)) = args_iter.peek() {
+            let mut folded_constants = *n;
+            args_iter.next();
+            while let Some(Expr::Constant(n)) = args_iter.peek() {
+                folded_constants = fold_op(fun, folded_constants, *n)?;
+                args_iter.next();
+            }
+            result.push(Expr::Constant(folded_constants));
+        }
+
+        for arg in args_iter {
+            if get_type(&arg) != data_type {
+                return None;
+            }
+            match arg {
+                Expr::Application(fun2, s) => {
+                    let folded_s = fold_constants(fun2, s)?;
+                    if folded_s.len() == 1 {
+                        result.push(folded_s[0].clone());
+                    } else {
+                        result.push(Expr::Application(*fun2, folded_s));
+                    }
+                },
+                _ => result.push(arg.clone()),
+            }
+        }
+
+        Some(result)
+    }
+}
+
+fn fold_all_constants_commutative(fun: &BuiltIn, args: &[Expr]) -> Option<Vec<Expr>> {
+    let mut applications = Vec::new();
+    let mut variables = Vec::new();
+    let mut folded_constants = None;
+
+    let data_type = get_type(&args[0]);
+
+    // Add all the variables to result and fold all constants into one
+    for arg in args {
+        if get_type(&arg) != data_type {
+            return None;
+        }
+        match arg {
+            Expr::Constant(n) => {
+                if let Some(folded_constants_n) = folded_constants {
+                    folded_constants = Some(fold_op(fun, folded_constants_n, *n)?);
+                } else {
+                    folded_constants = Some(*n);
+                }
+            },
+            Expr::Variable(n) => {
+                variables.push(Expr::Variable(*n));
+            },
+            Expr::Application(fun2, s) => {
+                let folded_s = fold_constants(fun2, s)?;
+                if folded_s.len() == 1 {
+                    match folded_s[0] {
+                        Expr::Constant(n) => {
+                            if let Some(folded_constants_n) = folded_constants {
+                                folded_constants = Some(fold_op(fun, folded_constants_n, n)?);
+                            } else {
+                                folded_constants = Some(n);
+                            }
+                        },
+                        Expr::Variable(n) => {
+                            variables.push(Expr::Variable(n));
+                        },
+                        _ => unreachable!()
+                    }
+                } else {
+                    applications.push(Expr::Application(*fun2, folded_s));
+                }
+            },
+        }
+    }
+
+    // We make sure to execute applications first then variables so that
+    // we minimize the number of times we need to take from the stack
+    let mut result = Vec::new();
+    result.extend(applications);
+    result.extend(variables);
+
+    if let Some(folded_constants_n) = folded_constants {
+        result.push(Expr::Constant(folded_constants_n));
+    }
+
+    Some(result)
+}
+
+#[derive(Debug)]
+pub enum CodeGenError {
+    Const(Atom),
+    TypeError
+}
+
+impl Display for CodeGenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CodeGenError::Const(n) => {
+                write!(f, "Result(const): {}", n)
+            },
+            CodeGenError::TypeError => {
+                write!(f, "Type error")
+            }
+        }
+    }
+}
+
+fn generate_atom<'cg>(cg: &'cg CodeGen, atom: &Atom) -> CGValueRef<'cg> {
+    match atom {
+        Atom::Num(n) => {
+            cg.new_i64_const(*n).into()
+        },
+        Atom::Boolean(b) => {
+            cg.new_bool_const(*b).into()
+        },
+    }
+}
+
+fn generate_int_op<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, left: I64Ref<'cg>, right: I64Ref<'cg>) -> I64Ref<'cg> {
+    match fun {
+        BuiltIn::Plus => {
+            left + &right
+        },
+        _ => todo!("For the moment only adds")
+        /*BuiltIn::Times => {
+            cp.generate_mul(data_type);
+        },
+        BuiltIn::Minus => {
+            cp.generate_sub(data_type);
+        },
+        BuiltIn::Divide => {
+            cp.generate_div(data_type);
+        },
+        BuiltIn::Equal => {
+            cp.generate_eq(data_type);
+        }*/
+    }
+}
+
+fn generate_bool_op<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, left: BoolRef<'cg>, right: BoolRef<'cg>) -> BoolRef<'cg> {
+    match fun {
+        /*
+        BuiltIn::Equal => {
+            left + &right
+        }*/
+        _ => todo!("For the moment no bools")
+    }
+}
+
+fn generate_code_application<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, args: &[Expr]) -> Result<CGValueRef<'cg>, CodeGenError> {
+    let first_variable = &args[0];
+
+    let mut cur = match first_variable {
+        Expr::Variable(n) => {
+            cg.get_arg(*n).into()
+        },
+        Expr::Constant(n) => {
+            generate_atom(cg, n)
+        },
+        Expr::Application(fun2, args2) => {
+            generate_code_application(cg,&fun2, &args2)?
+        },
+    };
+
+    for arg in args.iter().skip(1) {
+        let next: CGValueRef<'cg> = match arg {
+            Expr::Variable(n) => {
+                cg.get_arg(*n).into()
+            },
+            Expr::Constant(n) => {
+                generate_atom(cg, n)
+            },
+            Expr::Application(fun2, args2) => {
+                // Save the current result to the stack 
+                let folded_args = if let Some(fa) = fold_constants(fun2, args2) {
+                    fa
+                } else {
+                    return Err(CodeGenError::TypeError);
+                };
+                generate_code_application(cg, &fun2, &folded_args)?
+            },
+        };
+        match cur.data_type {
+            DataType::I64 => {
+                let left = I64Ref::from(cur);
+                let right = I64Ref::from(next);
+                cur = generate_int_op(cg, fun, left, right).into();
+            },
+            DataType::Bool => {
+                let left = BoolRef::from(cur);
+                let right = BoolRef::from(next);
+                cur = generate_bool_op(cg, fun, left, right).into();
+            },
+            _ => todo!("For the moment only int64s and bools")
+        }
+    }
+    Ok(cur)
+}
+
+fn generate_code_inner<'cg>(cg: &'cg CodeGen, expr: &Expr, args: usize) -> Result<CGValueRef<'cg>, CodeGenError> {
+    Ok(match expr {
+        Expr::Constant(e) => {
+            return Err(CodeGenError::Const(e.clone()));
+        },
+        Expr::Variable(n) => {
+            cg.get_arg(*n).into()
+        },
+        Expr::Application(fun, args) => {
+            let folded_args = if let Some(fa) = fold_constants(fun, args) {
+                fa
+            } else {
+                return Err(CodeGenError::TypeError);
+            };
+            if folded_args.len() == 1 {
+                match &folded_args[0] {
+                    // TODO: Once we introduce operators that also do something to
+                    //       a single argument, this is no longer correct like this
+                    //       but constant folding will need to be imroved anyway then
+                    Expr::Constant(n) => {
+                        return Err(CodeGenError::Const(n.clone()));
+                    },
+                    Expr::Variable(n) => {
+                        cg.get_arg(*n).into()
+                    },
+                    _ => {
+                        generate_code_application(cg, fun, &folded_args)?
+                    },
+                }
+            } else {
+                generate_code_application(cg, fun, &folded_args)?
+            }
+        },
+    })
+}
+
+pub fn generate_code(expr: &Expr, args: usize) -> Result<GeneratedCode, CodeGenError> {
+
+    let cg = CodeGen::new(args);
+
+    let return_val = generate_code_inner(&cg, expr, args)?;
+
+    cg.generate_return(&return_val);
+
+    let gc = cg.generate_code();
+
+    #[cfg(feature = "print-asm")]
+    {
+        let gc_code_slice = unsafe { std::slice::from_raw_parts(gc.code as *const u8, cg.code.len()) };
+        disassemble::disassemble(gc_code_slice);
+    }
+
+
+    Ok(gc)
+}
--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -3,15 +3,18 @@ pub mod stencils;
 pub mod ir;
 mod disassemble;
 mod copy_patch;
+mod expr_codegen;
 
 
-use std::{cell::RefCell, fmt::Display, hint::black_box, ops::Deref, ptr};
+use std::{cell::RefCell, hint::black_box, ops::Deref, ptr};
 use libc::c_void;
 use stencils::Stencil;
 
-use crate::{codegen::{copy_patch::STENCILS, ir::DataType}, expr::{Atom, BuiltIn, Expr}};
+use crate::codegen::{copy_patch::STENCILS, ir::DataType};
 
 use self::{copy_patch::CopyPatchBackend, ir::ConstValue};
+
+pub use expr_codegen::{get_type, generate_code, CodeGenError};
 
 
 
@@ -148,151 +151,7 @@ fn get_fn_ptr(f: unsafe extern "C" fn(*mut u8) -> *mut u8) -> *const c_void {
     f as *const c_void
 }
 
-fn fold_op(fun: &BuiltIn, l: Atom, r: Atom) -> Option<Atom> {
-    match (fun, l, r) {
-        (BuiltIn::Plus, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l + r)),
-        (BuiltIn::Times, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l * r)),
-        (BuiltIn::Minus, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l - r)),
-        (BuiltIn::Divide, Atom::Num(l), Atom::Num(r)) => Some(Atom::Num(l / r)),
-        (BuiltIn::Equal, Atom::Num(l), Atom::Num(r)) => Some(Atom::Boolean(l == r)),
-        (BuiltIn::Equal, Atom::Boolean(l), Atom::Boolean(r)) => Some(Atom::Boolean(l == r)),
-        _ => None,
-    }
-}
 
-fn is_commutative(fun: &BuiltIn) -> bool {
-    match fun {
-        BuiltIn::Plus | BuiltIn::Times | BuiltIn::Equal => true,
-        _ => false,
-    }
-}
-
-pub fn get_type(expr: &Expr) -> DataType {
-    match expr {
-        Expr::Constant(Atom::Num(_)) => DataType::I64,
-        Expr::Constant(Atom::Boolean(_)) => DataType::Bool,
-        Expr::Variable(_) => DataType::I64,
-        Expr::Application(fun, _) => {
-            match fun {
-                BuiltIn::Plus | BuiltIn::Minus | BuiltIn::Times | BuiltIn::Divide => {
-                    DataType::I64
-                },
-                BuiltIn::Equal => {
-                    DataType::Bool
-                }
-            }
-        },
-    }
-}
-
-fn fold_constants(fun: &BuiltIn, args: &[Expr]) -> Option<Vec<Expr>> {
-    if is_commutative(fun) {
-        fold_all_constants_commutative(fun, args)
-    } else {
-        // We just merge all the constants that appear in the beginning of the list for now which should be safe to do
-        // assuming that a list of arguments just means folding the arguments using the function
-        let mut result = Vec::new();
-
-        let mut args_iter = args.iter().peekable();
-
-        let data_type = get_type(&args[0]);
-        
-        if let Some(Expr::Constant(n)) = args_iter.peek() {
-            let mut folded_constants = *n;
-            args_iter.next();
-            while let Some(Expr::Constant(n)) = args_iter.peek() {
-                folded_constants = fold_op(fun, folded_constants, *n)?;
-                args_iter.next();
-            }
-            result.push(Expr::Constant(folded_constants));
-        }
-
-        for arg in args_iter {
-            if get_type(&arg) != data_type {
-                return None;
-            }
-            match arg {
-                Expr::Application(fun2, s) => {
-                    let folded_s = fold_constants(fun2, s)?;
-                    if folded_s.len() == 1 {
-                        result.push(folded_s[0].clone());
-                    } else {
-                        result.push(Expr::Application(*fun2, folded_s));
-                    }
-                },
-                _ => result.push(arg.clone()),
-            }
-        }
-
-        Some(result)
-    }
-}
-
-fn fold_all_constants_commutative(fun: &BuiltIn, args: &[Expr]) -> Option<Vec<Expr>> {
-    let mut applications = Vec::new();
-    let mut variables = Vec::new();
-    let mut folded_constants = None;
-
-    let data_type = get_type(&args[0]);
-
-    // Add all the variables to result and fold all constants into one
-    for arg in args {
-        if get_type(&arg) != data_type {
-            return None;
-        }
-        match arg {
-            Expr::Constant(n) => {
-                if let Some(folded_constants_n) = folded_constants {
-                    folded_constants = Some(fold_op(fun, folded_constants_n, *n)?);
-                } else {
-                    folded_constants = Some(*n);
-                }
-            },
-            Expr::Variable(n) => {
-                variables.push(Expr::Variable(*n));
-            },
-            Expr::Application(fun2, s) => {
-                let folded_s = fold_constants(fun2, s)?;
-                if folded_s.len() == 1 {
-                    match folded_s[0] {
-                        Expr::Constant(n) => {
-                            if let Some(folded_constants_n) = folded_constants {
-                                folded_constants = Some(fold_op(fun, folded_constants_n, n)?);
-                            } else {
-                                folded_constants = Some(n);
-                            }
-                        },
-                        Expr::Variable(n) => {
-                            variables.push(Expr::Variable(n));
-                        },
-                        _ => unreachable!()
-                    }
-                } else {
-                    applications.push(Expr::Application(*fun2, folded_s));
-                }
-            },
-        }
-    }
-
-    // We make sure to execute applications first then variables so that
-    // we minimize the number of times we need to take from the stack
-    let mut result = Vec::new();
-    result.extend(applications);
-    result.extend(variables);
-
-    if let Some(folded_constants_n) = folded_constants {
-        result.push(Expr::Constant(folded_constants_n));
-    }
-
-    Some(result)
-}
-
-fn atom_to_const_value(atom: &Atom) -> ConstValue {
-    match atom {
-        Atom::Num(n) => ConstValue::I64(*n),
-        Atom::Boolean(b) => ConstValue::Bool(*b),
-    }
-}
 
 // We try to write a nice rustic API here that is easy to use
 // and takes advantage of the borrow checker to make sure that
@@ -486,6 +345,7 @@ impl CodeGen {
         cg.add(l.i, r.i)
     }
 
+    #[allow(dead_code)]
     pub fn reset(&mut self) {
         self.inner.borrow_mut().reset();
     }
@@ -533,6 +393,7 @@ impl CodeGenInner {
         }
     }
 
+    #[allow(dead_code)]
     fn reset(&mut self) {
         self.reg_state = [None, None];
         self.values.clear();
@@ -616,7 +477,7 @@ impl CodeGenInner {
         let value = &self.values[v];
         // Load the value into the first scratch register
         match value {
-            CGValue::Variable{data_type, stack_pos, ..} => {
+            CGValue::Variable{stack_pos, ..} => {
                 // TODO: do take stack with datatype
                 self.inner.generate_take_2_stack(*stack_pos);
             },
@@ -663,7 +524,11 @@ impl CodeGenInner {
         None
     }
 
+    #[allow(dead_code)]
     fn dirty_reg2(&mut self) -> Option<usize> {
+        // This case is non-trivial because we also have to handle the case where value is already in reg1 and is dirty
+        // In this case we could have two diverging states for the same value
+        // Maybe this can't even happen as long as we keep the "one reference per mutable value" invariant
         todo!("Implement dirtying of second register")
     }
 
@@ -771,248 +636,3 @@ impl CodeGenInner {
     }
 }
 
-#[derive(Debug)]
-pub enum CodeGenError {
-    Const(Atom),
-    TypeError
-}
-
-impl Display for CodeGenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            CodeGenError::Const(n) => {
-                write!(f, "Result(const): {}", n)
-            },
-            CodeGenError::TypeError => {
-                write!(f, "Type error")
-            }
-        }
-    }
-}
-
-fn generate_atom<'cg>(cg: &'cg CodeGen, atom: &Atom) -> CGValueRef<'cg> {
-    match atom {
-        Atom::Num(n) => {
-            cg.new_i64_const(*n).into()
-        },
-        Atom::Boolean(b) => {
-            cg.new_bool_const(*b).into()
-        },
-    }
-}
-
-fn generate_int_op<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, left: I64Ref<'cg>, right: I64Ref<'cg>) -> I64Ref<'cg> {
-    match fun {
-        BuiltIn::Plus => {
-            left + &right
-        },
-        _ => todo!("For the moment only adds")
-        /*BuiltIn::Times => {
-            cp.generate_mul(data_type);
-        },
-        BuiltIn::Minus => {
-            cp.generate_sub(data_type);
-        },
-        BuiltIn::Divide => {
-            cp.generate_div(data_type);
-        },
-        BuiltIn::Equal => {
-            cp.generate_eq(data_type);
-        }*/
-    }
-}
-
-fn generate_bool_op<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, left: BoolRef<'cg>, right: BoolRef<'cg>) -> BoolRef<'cg> {
-    match fun {
-        /*
-        BuiltIn::Equal => {
-            left + &right
-        }*/
-        _ => todo!("For the moment no bools")
-    }
-}
-
-fn generate_code_application<'cg>(cg: &'cg CodeGen, fun: &BuiltIn, args: &[Expr]) -> Result<CGValueRef<'cg>, CodeGenError> {
-    let first_variable = &args[0];
-
-    let mut cur = match first_variable {
-        Expr::Variable(n) => {
-            cg.get_arg(*n).into()
-        },
-        Expr::Constant(n) => {
-            generate_atom(cg, n)
-        },
-        Expr::Application(fun2, args2) => {
-            generate_code_application(cg,&fun2, &args2)?
-        },
-    };
-
-    for arg in args.iter().skip(1) {
-        let next: CGValueRef<'cg> = match arg {
-            Expr::Variable(n) => {
-                cg.get_arg(*n).into()
-            },
-            Expr::Constant(n) => {
-                generate_atom(cg, n)
-            },
-            Expr::Application(fun2, args2) => {
-                // Save the current result to the stack 
-                let folded_args = if let Some(fa) = fold_constants(fun2, args2) {
-                    fa
-                } else {
-                    return Err(CodeGenError::TypeError);
-                };
-                generate_code_application(cg, &fun2, &folded_args)?
-            },
-        };
-        match cur.data_type {
-            DataType::I64 => {
-                let left = I64Ref::from(cur);
-                let right = I64Ref::from(next);
-                cur = generate_int_op(cg, fun, left, right).into();
-            },
-            DataType::Bool => {
-                let left = BoolRef::from(cur);
-                let right = BoolRef::from(next);
-                cur = generate_bool_op(cg, fun, left, right).into();
-            },
-            _ => todo!("For the moment only int64s and bools")
-        }
-    }
-    Ok(cur)
-}
-
-fn generate_code_inner<'cg>(cg: &'cg CodeGen, expr: &Expr, args: usize) -> Result<CGValueRef<'cg>, CodeGenError> {
-    Ok(match expr {
-        Expr::Constant(e) => {
-            return Err(CodeGenError::Const(e.clone()));
-        },
-        Expr::Variable(n) => {
-            cg.get_arg(*n).into()
-        },
-        Expr::Application(fun, args) => {
-            let folded_args = if let Some(fa) = fold_constants(fun, args) {
-                fa
-            } else {
-                return Err(CodeGenError::TypeError);
-            };
-            if folded_args.len() == 1 {
-                match &folded_args[0] {
-                    // TODO: Once we introduce operators that also do something to
-                    //       a single argument, this is no longer correct like this
-                    //       but constant folding will need to be imroved anyway then
-                    Expr::Constant(n) => {
-                        return Err(CodeGenError::Const(n.clone()));
-                    },
-                    Expr::Variable(n) => {
-                        cg.get_arg(*n).into()
-                    },
-                    _ => {
-                        generate_code_application(cg, fun, &folded_args)?
-                    },
-                }
-            } else {
-                generate_code_application(cg, fun, &folded_args)?
-            }
-        },
-    })
-}
-
-pub fn generate_code(expr: &Expr, args: usize) -> Result<GeneratedCode, CodeGenError> {
-
-    let cg = CodeGen::new(args);
-
-    let return_val = generate_code_inner(&cg, expr, args)?;
-
-    cg.generate_return(&return_val);
-
-    let gc = cg.generate_code();
-
-    #[cfg(feature = "print-asm")]
-    {
-        let gc_code_slice = unsafe { std::slice::from_raw_parts(gc.code as *const u8, cg.code.len()) };
-        disassemble::disassemble(gc_code_slice);
-    }
-
-
-    Ok(gc)
-}
-
-
-//let mut cg = CopyPatchBackend::new(args);
-
-    //let ghc_stencil = STENCILS.get(&StencilType::new(StencilOperation::GhcWrapper, None)).unwrap();
-
-    // We put this here just to demonstrate the capabilities to call back into pre-compiled functions
-    // and to demonstrate ifs
-    // Uncomment this if you want to try it out:
-    //#[cfg(not(test))]
-    /*if let Expr::Application(BuiltIn::Divide, args) = expr{
-        cg.generate_take_stack(0);
-        cg.generate_rem_const(ConstValue::I64(2));
-        cg.generate_eq_const(ConstValue::I64(0));
-        cg.generate_if_else(|cg: &mut CodeGen| {
-            cg.generate_take_const(ConstValue::I64(123));
-            cg.generate_put_stack(args * 8);
-            cg.generate_get_stackptr(args * 8);
-            cg.generate_call_c_func(get_fn_ptr(hello_world));
-        }, |cg| {
-            cg.generate_take_const(ConstValue::I64(321));
-            cg.generate_put_stack(args * 8);
-            cg.generate_get_stackptr(args * 8);
-            cg.generate_call_c_func(get_fn_ptr(hello_world));
-        });
-    }*/
-
-    
-    /*match expr {
-        Expr::Constant(e) => {
-            //cg.generate_atom(e);
-        },
-        Expr::Variable(n) => {
-            //cg.generate_take_stack(n * 8);
-        },
-        Expr::Application(fun, args) => {
-            let folded_args = if let Some(fa) = fold_constants(fun, args) {
-                fa
-            } else {
-                return Err(CodeGenError::TypeError);
-            };
-            if folded_args.len() == 1 {
-                match &folded_args[0] {
-                    // TODO: Once we introduce operators that also do something to
-                    //       a single argument, this is no longer correct like this
-                    //       but constant folding will need to be imroved anyway then
-                    Expr::Constant(n) => {
-                        return Err(CodeGenError::Const(n.clone()));
-                    },
-                    Expr::Variable(n) => {
-                        //cg.generate_take_stack(n * 8);
-                    },
-                    _ => {
-                        //cg.generate_code_application(fun, &folded_args);
-                    },
-                }
-            } else {
-                //cg.generate_code_application(fun, &folded_args);
-            }
-        },
-    }*/
-
-    // Put the result back on the stack and return
-    //cg.generate_put_stack(0);
-    //cg.generate_ret();
-
-    //let gc = GeneratedCode::<T>::new(cg.stack_size * 8, ghc_stencil, &cg.code);
-
-    // Fix up the holes that need an absolute address
-
-    // TODO: Find a nicer solution for this
-    /*for &ofs in cg.fixup_holes.iter() {
-        unsafe {
-            let addr = gc.code.byte_offset(ofs as isize) as *mut u64;
-            let start_offset = addr.read_unaligned();
-            let new_addr = gc.code as u64 + start_offset;
-            addr.write_unaligned(new_addr);           
-        }
-    }*/
